0633d164f46b3ab5293dd65ebebe07a7e522ed0d
dameem4 <dameem_s@yahoo.com>
2024-03-10 17:05:00 +0000

    Update layout.js

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import "../styles/layout.css" // Import your CSS file for layout styles
+import "../layout.css" // Import your CSS file for layout styles
 
 const Layout = ({ children }) => (
   <div>
